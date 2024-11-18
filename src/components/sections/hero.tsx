f352f4cf05048150ef1906821a50948cfc7084ef
Abdelrahman mohamed <abdelrahmanelnobby@gmail.com>
2024-11-18 18:47:20 +0200

    updates

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -113,7 +113,7 @@ export const Hero = ({ texts }: Props) => {
           <div className="flex items-center gap-4 mt-4">
             <Button
               onClick={() => {
-                downloadFile("/assets/cv.pdf", "Abdelrahman mohamed.pdf");
+                downloadFile("/assets/cv.pdf", "abdelrahman mohamed.pdf");
               }}
               data-tooltip-id="my-tooltip"
               data-tooltip-content="CV"
